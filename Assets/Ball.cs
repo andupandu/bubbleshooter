1f58e444881a1e41c10271f77cf8bbfb1005f526
Limba Andreea <andreea.limba@yahoo.com>
2017-05-24 18:08:56 +0300

    Move ball on click

---
+++ Assets/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Ball : MonoBehaviour
+    {
+        enum Status
+        {
+            INITIAL, THROWN, FINAL
+        }
+
+        Status status = Status.INITIAL;
+        Vector3 vectorTO = new Vector3(0, 0, 0);
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (status == Status.INITIAL && Input.GetMouseButton(0))
+            {
+                {
+                    vectorTO = Utils.V3ToV2(Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Utils.V3ToV2(transform.position);
+                    status = Status.THROWN;
+                }
+            }
+
+            if (status == Status.THROWN)
+            {
+                this.transform.Translate(vectorTO * Time.deltaTime);
+            }
+        }
+
+    }
+}
---
+++ Assets/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    static class Utils
+    {
+        public static Vector2 V3ToV2(Vector3 v)
+        {
+            return new Vector2(v.x, v.y);
+        }
+    }
+}
